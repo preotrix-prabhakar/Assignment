d7b6aa77a42cc4529d3cb7f4e4d00e457ad4ce83
preotrix-prabhakar <prabhakar8859@gmail.com>
2024-07-19 00:34:49 +0530

    front-end work

---
+++ script.js
@@ -0,0 +1,21 @@
+let paper=document.getElementById('paper');
+let V=document.getElementById('V');
+let fist=document.getElementById('fist');
+let rules=document.getElementById('rules');
+
+
+
+
+
+rules.addEventListener('click',onlick=>{
+alert("rules buttonn is clicked");
+})
+V.addEventListener('click',onlick=>{
+alert("rules buttonn is clicked");
+})
+paper.addEventListener('click',onlick=>{
+alert("rules buttonn is clicked");
+})
+fist.addEventListener('click',onlick=>{
+alert("rules buttonn is clicked");
+})
